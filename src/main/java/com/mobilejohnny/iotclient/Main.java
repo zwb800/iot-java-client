37d8bf297d7eabceb9f90d1522d8fe12e84172eb
zwb800 <zwb800@gmail.com>
2015-08-18 00:03:59 +0800

    restful api

--- src/main/java/com/mobilejohnny/iotclient/Main.java
+++ src/main/java/com/mobilejohnny/iotclient/Main.java
@@ -17,7 +17,7 @@ import java.net.MalformedURLException;
 public class Main {
 
 
-
+//http://rest.yunba.io:8080/?method=publish_to_alias&appkey=55c8c6339477ebf5246956dc&seckey=sec-smA1Ze8y1pZC0qAELDbrjuDnHAaJvMeutk4L6WTU8YQU0ewc&alias=1&msg=111
     public static void main(String[] args) {
         System.out.println("Starting...");
         boolean result = SerialComm.connect("/dev/ttyUSB0");
@@ -38,17 +38,17 @@ public class Main {
                 }
             });
 
-            new Thread(new Runnable() {
-                @Override
-                public void run() {
-                    try {
-                        Thread.sleep(120000);
-                        System.exit(0);
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
-                }
-            }).start();
+//            new Thread(new Runnable() {
+//                @Override
+//                public void run() {
+//                    try {
+//                        Thread.sleep(120000);
+//                        System.exit(0);
+//                    } catch (InterruptedException e) {
+//                        e.printStackTrace();
+//                    }
+//                }
+//            }).start();
         }
     }
 }
